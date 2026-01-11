6eeff225dc967f854d16f83e83287bbe0624309a
Aadesh Dalvi <adeshdalvi41@gmail.com>
2026-01-11 10:18:27 +0530

    Initial commit

---
+++ src/main/java/com/cafe/kaffeine/KaffeineApplication.java
@@ -0,0 +1,13 @@
+package com.cafe.kaffeine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaffeineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaffeineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cafe/kaffeine/KaffeineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cafe.kaffeine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaffeineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
